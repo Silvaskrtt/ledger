efd8ec6521369133fcd0fbdf904e4f2afe5b8a54
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2025-12-26 21:19:54 -0300

    feat: I added 'transaction' management views for system.

--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -1,7 +1,45 @@
+from django.shortcuts import render
+from django.views import View
 from rest_framework import viewsets
+
+from .forms import TransactionForm
 from .models import Transaction, TransactionAccount, TransactionTag
 from .serializers import TransactionSerializer, TransactionAccountSerializer, TransactionTagSerializer
 
+class TransactionView(View):
+    template_name = 'transactions/transaction.html' # ainda nao tem frontend
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {
+            'form': TransactionForm(),
+            # ainda falta implementar mais funcionalidades
+            'transactions': Transaction.objects.all()
+        })
+        
+    def post(self, request, *args, **kwargs):
+        print("Debug: Recebido POST em TransactionView")
+        
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # redirecionar ou renderizar com sucesso
+        return render(request, self.template_name, {
+            'form': form,
+            'transactions': Transaction.objects.all()
+        })
+        
+        if not form.is_valid():
+            print("Debug: Formulário inválido")
+            print(form.errors)
+            return render(request, self.template_name, {
+                'form': form, })
+
+        transaction = form.save()
+        
+        return render(request, self.template_name, {
+            'form': TransactionForm(),
+            'transactions': Transaction.objects.all()
+        })
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
