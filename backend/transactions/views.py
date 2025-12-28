7a24da03526563cf559ecc2017ae7bf7d952d5d0
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2025-12-28 00:02:26 -0300

    feat: I added context for fields in system.

--- backend/transactions/views.py
+++ backend/transactions/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Transaction, TransactionAccount, TransactionTag
+from .models import Transaction, TransactionAccount, TransactionTag, Category, PaymentMethod, Account, Tag
 from .serializers import TransactionSerializer, TransactionAccountSerializer, TransactionTagSerializer
 from django.views.generic import TemplateView
 
@@ -8,7 +8,11 @@ class CreateTransactionView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['payment_methods'] = PaymentMethod.objects.all()
         context['transaction_id'] = kwargs.get('pk', None)
+        context['accounts'] = Account.objects.all()
+        context['tags'] = Tag.objects.all()
         return context
 
 class TransactionListCreateView(generics.ListCreateAPIView):
