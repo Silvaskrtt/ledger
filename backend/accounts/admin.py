7b8469f5d11075c7ad31c50d1de7140e2db1bdd3
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2025-12-29 20:50:42 -0300

    feat: We've added new fields to the admin panel view.

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -4,5 +4,5 @@ from .models import Account
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('id_account', 'type', 'active', 'id_user')
+    list_display = ('id_account', 'balance', 'type', 'active', 'id_user')
     search_fields = ('id_user__email',)
