27b4edfa81c56cb24b060c7bfd21dec7f1ab2b75
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-02 11:15:32 -0300

    chore: just changes in routes.

--- backend/home/urls_web.py
+++ backend/home/urls_web.py
@@ -1,8 +1,8 @@
+# backend/home/urls_web.py
 from django.urls import path
-
 from .views import (teste_html_view,)
 
 urlpatterns = [
     #HOME
-    path("home/", teste_html_view, name="teste_html")
+    path("teste-html/", teste_html_view, name="teste_html")
 ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,6 +1,7 @@
-"""Views para análise de dashboards."""
+# backend/home/views.py
+
 from django.shortcuts import render
 
 def teste_html_view(request):
-    """View para renderizar o template de teste de HTML."""
+    # View para renderizar o template de teste de HTML.
     return render(request, 'home/home.html', {})
