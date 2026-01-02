643b608ec37887a8088d041c81acffd6da82d590
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-02 11:21:15 -0300

    feat & docs: New settings for the login. The other changes I'm making are in the comments of the necessary lines.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,4 @@
-# users/views.py
+# backend/users/views.py
 """Views para autenticação e gerenciamento de usuários."""
 
 from django.shortcuts import render, redirect
@@ -8,12 +8,16 @@ from django.contrib import messages
 @login_required
 def home(request):
     """Página inicial após login."""
-    # Agora o usuário é do modelo User padrão do Django
-    user_name = request.user.get_full_name() or request.user.username
-    return render(request, 'home/home.html', {
-        'user_name': user_name
-    })
-
+    if request.user.is_authenticated:
+        # Se já está logado, vai para home
+        user_name = request.user.get_full_name() or request.user.username
+        return render(request, 'home/home.html', {
+            'user_name': user_name
+        })
+    else:
+        # Se não está logado, redireciona para login
+        return redirect('account_login')
+    
 @login_required
 def profile_view(request):
     """Página de perfil do usuário."""
