b38ada99e8e650963016a2bc116268c4aab75664
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-06 12:56:13 -0300

    feat: new routes for the API for the goals.

---
+++ backend/goals/urls_api.py
@@ -0,0 +1,9 @@
+# backend/goals/urls_api.py
+
+from django.urls import path
+from .views import FinancialGoalListCreateView, FinancialGoalDetailView
+
+urlpatterns = [
+    path('financial-goals/', FinancialGoalListCreateView.as_view(), name='financial-goal-list'),
+    path('financial-goals/<uuid:pk>/', FinancialGoalDetailView.as_view()),
+]
