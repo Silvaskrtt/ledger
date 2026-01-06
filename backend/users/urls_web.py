68abbda1d925c7db2c52f79395f555381b51dcaf
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-06 13:11:52 -0300

    fix: correcting route.

--- backend/users/urls_web.py
+++ backend/users/urls_web.py
@@ -4,8 +4,6 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Página inicial após login
-    path('home/', views.home, name='home'),
     path('profile/', views.profile_view, name='profile'),
     
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
