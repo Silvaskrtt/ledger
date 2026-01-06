a5bbcd2a270e07405f377a8b028a5612e2b26e1f
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-06 12:59:51 -0300

    feat: new rules for the View for the goals.

--- backend/goals/views.py
+++ backend/goals/views.py
@@ -1,13 +1,29 @@
+# backend/goals/views.py
+
 from rest_framework import generics
 from .models import FinancialGoal
 from .serializers import FinancialGoalSerializer
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError
 
 class FinancialGoalListCreateView(generics.ListCreateAPIView):
-    queryset = FinancialGoal.objects.all()
     serializer_class = FinancialGoalSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return FinancialGoal.objects.filter(id_user=self.request.user)
 
+    def perform_create(self, serializer):
+        serializer.save(id_user=self.request.user)                
 
 class FinancialGoalDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = FinancialGoal.objects.all()
     serializer_class = FinancialGoalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return FinancialGoal.objects.filter(id_user=self.request.user)
+
+    def perform_update(self, serializer):
+        if self.get_object().is_cancelled:
+            raise ValidationError("Meta cancelada não pode ser alterada.")
+        serializer.save()
