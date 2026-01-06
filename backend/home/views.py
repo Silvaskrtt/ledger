a33874f31c0e99deaa9b08416bacdd56fa79cc8d
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-06 13:11:18 -0300

    feat: Added business rules.

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,7 +1,50 @@
-# backend/home/views.py
-
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
+from django.utils.timezone import now
 
-def teste_html_view(request):
-    # View para renderizar o template de teste de HTML.
-    return render(request, 'home/home.html', {})
+from accounts.models import Account
+from transactions.models import Transaction
+
+
+@login_required
+def home_view(request):
+    user = request.user
+    today = now()
+
+    saldo_atual = (
+        Account.objects
+        .filter(user=user)
+        .aggregate(total=Sum('balance'))['total']
+        or 0
+    )
+
+    entradas_mes = (
+        Transaction.objects
+        .filter(
+            id_user=user,
+            direction='IN',
+            occurred_at__year=today.year,
+            occurred_at__month=today.month
+        )
+        .aggregate(total=Sum('amount'))['total']
+        or 0
+    )
+
+    saidas_mes = (
+        Transaction.objects
+        .filter(
+            id_user=user,
+            direction='OUT',
+            occurred_at__year=today.year,
+            occurred_at__month=today.month
+        )
+        .aggregate(total=Sum('amount'))['total']
+        or 0
+    )
+
+    return render(request, 'home/home.html', {
+        'saldo_atual': saldo_atual,
+        'entradas_mes': entradas_mes,
+        'saidas_mes': saidas_mes,
+    })
