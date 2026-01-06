0da51ff0fc87ad94057d31135db2e90520de92b5
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-06 12:55:24 -0300

    feat: rules for the finance.

--- backend/goals/serializers.py
+++ backend/goals/serializers.py
@@ -1,9 +1,36 @@
+# backend/goals/serializers.py
+
 from rest_framework import serializers
+from datetime import date
 from .models import FinancialGoal
 
-
 class FinancialGoalSerializer(serializers.ModelSerializer):
+    percent = serializers.ReadOnlyField()
+    status = serializers.ReadOnlyField()
+
     class Meta:
         model = FinancialGoal
-        fields = ['id_financial_goal', 'id_user', 'name', 'target_amount', 'deadline', 'strategy', 'status']
-        read_only_fields = ['id_financial_goal']
+        fields = [
+            'id_financial_goal',
+            'name',
+            'target_amount',
+            'current_amount',
+            'deadline',
+            'strategy',
+            'status',
+            'percent'
+        ]
+
+    def validate_current_amount(self, value):
+        if value < 0:
+            raise serializers.ValidationError(
+                "Valor acumulado não pode ser negativo."
+            )
+        return value
+
+    def validate_deadline(self, value):
+        if value < date.today():
+            raise serializers.ValidationError(
+                "Deadline deve ser uma data futura."
+            )
+        return value
