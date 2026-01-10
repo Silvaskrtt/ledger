57dc3eafa66cc45fa3a48475854e908500a99bf7
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-09 23:05:45 -0300

    test: route for the something test for HTML and css

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import AccountListCreateView, AccountDetailView
 
+from .views import credit_cards_view
+
 urlpatterns = [
     path('accounts/', AccountListCreateView.as_view(), name='account-list'),
     path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account-detail'),
+    
+    path('cartoes/', credit_cards_view, name='credit-cards'),
 ]
