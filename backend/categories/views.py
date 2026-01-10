9e8ea6b956e791b8cff26f77e6fea09540f35ed6
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-01-10 11:56:38 -0300

    feat: views for the routes and rules business.

--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,10 +1,17 @@
+# backend/categories/views.py
+
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Category
+from .serializers import CategorySerializer
+
 """Views para gerenciamento de categorias."""
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Category
 from .serializers import CategorySerializer
 from django.shortcuts import render
-
 
 class CategoryListCreateView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
@@ -15,6 +22,11 @@ class CategoryListCreateView(generics.ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(id_user=self.request.user)
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
 
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -24,5 +36,11 @@ class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Category.objects.filter(id_user=self.request.user)
     
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+
+
 def categories_management_view(request):
     return render(request, 'categories_management/categories_management.html')
