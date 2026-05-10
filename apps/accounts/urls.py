b467e4ac0985479042e45253eb87b85da3f26a8e
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-05-10 09:55:43 -0300

    config: configuraçoes de rotas.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('profile/update-ajax/', views.profile_update_ajax, name='profile_update_ajax'),
+    path('profile/', views.profile_view, name='profile'),
+    path('profile/update/', views.profile_update, name='profile_update'),
+    path('avatar/upload/', views.avatar_upload, name='avatar_upload'),
+    path('password/change/', views.password_change, name='password_change'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', login_required(TemplateView.as_view(template_name='home.html')), name='home'),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('profile/', include('accounts.urls')),
 ]
+
+# Servir arquivos de mídia em desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
